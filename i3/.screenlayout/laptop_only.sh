0e3909a0c30e56454205da886a88bdf7f95c5a38
Dave Seleno <onelesd@users.noreply.github.com>
2019-06-05 15:04:15 -0700

    new screen layout

--- i3/.screenlayout/laptop_only.sh
+++ i3/.screenlayout/laptop_only.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 xrandr --output VIRTUAL1 --off \
-       --output eDP1 --off \
-       --output DP1 --primary \
-       --mode 3440x1440 --pos 0x0 --rotate normal \
+       --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+       --output DP1 --off \
        --output HDMI2 --off \
        --output HDMI1 --off \
        --output DP2 --off
