0d8f02ba5601320ba21d45e3771a898e70534be9
Dave Seleno <dave.seleno@nike.com>
2022-11-05 19:33:33 -0700

    40 inch lg monitor

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -3,10 +3,20 @@ hs.window.animationDuration = 0
 
 hs.loadSpoon('WindowHalfsAndThirds')
 
-hs.grid.setGrid('2x2', '2560x1600')
+hs.grid.setGrid('9x5', '5120x2160')
+hs.grid.HINTS={
+    {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'},
+    {'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'},
+    {'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1'},
+    {'2', '3', '4', '5', '6', '7', '8', '9', '0'},
+    {'-', '=', ',', '.', '[', ']', '`', '/', '\\'}
+}
 hs.grid.setMargins({0, 0})
 
 local mash = {"cmd", "alt", "ctrl"}
+local meh = {"ctrl", "alt", "shift"}
+local audio = require 'hs.audiodevice'
+local volume = {}
 
 spoon.WindowHalfsAndThirds:bindHotkeys({
     max_toggle  = { mash, "M" },
@@ -14,9 +24,9 @@ spoon.WindowHalfsAndThirds:bindHotkeys({
     bottom_half = { mash, "Down" },
     left_half   = { mash, "Left" },
     right_half  = { mash, "Right" },
-    larger      = { mash, "PageUp" },
-    smaller     = { mash, "PageDown" },
-    third_up    = { mash, "Return" },
+    -- larger      = { mash, "PageUp" },
+    -- smaller     = { mash, "PageDown" },
+    -- third_up    = { mash, "Return" },
 })
 
 -- Like hs.application.launchOrFocus, except that it works for apps created using Epichrome.
@@ -52,19 +62,75 @@ hs.hotkey.bind(mash, "Space", function()
 end)
 
 -- send window to center at half screen width @ 16:9 ratio
-hs.hotkey.bind(mash, "Delete", function()
+hs.hotkey.bind(mash, "Return", function()
     centerFocusedWindow()
 end)
 
--- send window to next screen
-hs.hotkey.bind(mash, "N", function()
-    local win = hs.window.focusedWindow()
-    local screen = win:screen()
+-- volume helper
+function changeVolume(diff)
+  return function()
+    local current = hs.audiodevice.defaultOutputDevice():volume()
+    local new = math.min(100, math.max(0, math.floor(current + diff)))
+    if new > 0 then
+      hs.audiodevice.defaultOutputDevice():setMuted(false)
+    end
+    hs.alert.closeAll(0.0)
+    hs.alert.show("Volume " .. new .. "%", {}, 0.5)
+    hs.audiodevice.defaultOutputDevice():setVolume(new)
+  end
+end
 
-    win:setFullScreen(false)
-    -- move to next screen, don't resize, make sure the window fits on screen
-    win:moveToScreen(screen:next(), true, true)
+-- volume
+function volume.mute()
+    local dev = audio.defaultOutputDevice()
+    return dev and dev:setMuted(true)
+end
+
+function volume.unmute()
+    local dev = audio.defaultOutputDevice()
+    return dev and dev:setMuted(false)
+end
+
+function volume.muted()
+    local dev = audio.defaultOutputDevice()
+    return dev and dev:muted()
+end
+
+hs.hotkey.bind(mash, '0', function()
+  if volume.muted() then
+    volume.unmute()
+  else
+    volume.mute()
+  end
 end)
+hs.hotkey.bind(mash, '-', changeVolume(-3))
+hs.hotkey.bind(mash, '=', changeVolume(3))
+
+-- media keys
+hs.hotkey.bind(mash, "/", function()
+  hs.eventtap.event.newSystemKeyEvent('PLAY', true):post()
+  hs.eventtap.event.newSystemKeyEvent('PLAY', false):post()
+end)
+
+hs.hotkey.bind(mash, ".", function()
+  hs.eventtap.event.newSystemKeyEvent('NEXT', true):post()
+  hs.eventtap.event.newSystemKeyEvent('NEXT', false):post()
+end)
+
+hs.hotkey.bind(mash, ",", function()
+  hs.eventtap.event.newSystemKeyEvent('PREVIOUS', true):post()
+  hs.eventtap.event.newSystemKeyEvent('PREVIOUS', false):post()
+end)
+
+-- send window to next screen
+-- hs.hotkey.bind(mash, "N", function()
+--     local win = hs.window.focusedWindow()
+--     local screen = win:screen()
+--
+--     win:setFullScreen(false)
+--     -- move to next screen, don't resize, make sure the window fits on screen
+--     win:moveToScreen(screen:next(), true, true)
+-- end)
 
 hs.hotkey.bind(mash, "F", function()
     finder = hs.appfinder.appFromName("Finder")
@@ -81,36 +147,27 @@ hs.hotkey.bind(mash, "F", function()
     finder:activate()
 end)
 
-hs.hotkey.bind(mash, "1", function()
-    myLaunchOrFocus("1Password 7")
-end)
-
-hs.hotkey.bind(mash, "P", function()
-    myLaunchOrFocus("Plex Media Player")
-end)
-
-hs.hotkey.bind(mash, "C", function()
-    myLaunchOrFocus("Google Chrome")
-end)
-
-hs.hotkey.bind(mash, "I", function()
-    myLaunchOrFocus("Messages")
-end)
-
 hs.hotkey.bind(mash, "T", function()
     myLaunchOrFocus("iTerm")
 end)
 
-hs.hotkey.bind(mash, "E", function()
-    myLaunchOrFocus("/Applications/Emacs.app")
-end)
-
 hs.hotkey.bind(mash, "S", function()
-    myLaunchOrFocus("Spotify")
+    myLaunchOrFocus("Slack")
+end)
+hs.hotkey.bind(meh, "S", function()
+    myLaunchOrFocus("Slack")
 end)
 
 hs.hotkey.bind(mash, "O", function()
-    myLaunchOrFocus("Organizr")
+    myLaunchOrFocus("Microsoft Outlook")
+end)
+
+hs.hotkey.bind(mash, "N", function()
+    myLaunchOrFocus("Obsidian")
+end)
+
+hs.hotkey.bind(mash, "I", function()
+    myLaunchOrFocus("Messages")
 end)
 
 -- reload config
